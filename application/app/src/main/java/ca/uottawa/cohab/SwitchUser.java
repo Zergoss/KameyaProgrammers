ff8ff9f90d7df422ee0cb6ba4eff39d78d1ed888
zergoss <rroyl091@uottawa.ca>
2017-11-22 17:08:44 -0500

    Creation of sign in and main activity interface

---
+++ application/app/src/main/java/ca/uottawa/cohab/SwitchUser.java
@@ -0,0 +1,26 @@
+package ca.uottawa.cohab;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrateur on 2017-11-22.
+ */
+
+public class SwitchUser extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.content_switch_user,container, false);
+        return myView;
+    }
+
+}
