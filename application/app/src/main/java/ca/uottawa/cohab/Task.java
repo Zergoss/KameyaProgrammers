c5dfdabcd082b5b6b6ecc9d4440970e3929f0a26
zergoss <rroyl091@uottawa.ca>
2017-11-24 05:05:49 -0500

    Task class 1

--- application/app/src/main/java/ca/uottawa/cohab/Task.java
+++ application/app/src/main/java/ca/uottawa/cohab/Task.java
@@ -1,8 +1,36 @@
 package ca.uottawa.cohab;
 
+import java.util.Date;
+
 /**
  * Created by Administrateur on 2017-11-23.
  */
 
 public class Task {
+    private String name;
+    private String description;
+    private Integer points;  //Real???
+    private Date startDate;
+    private Date endDate;
+    private Date dueDate;
+    private boolean status;
+
+    boolean checkStatus (){
+        return this.status;
+    }
+
+    class Taches{
+
+
+        String nom;
+        double StartTime;
+        double endTime;
+        String description;
+
+
+        String status (){ // tache est disponible ou non
+            return
+        }
+
+    }
 }
