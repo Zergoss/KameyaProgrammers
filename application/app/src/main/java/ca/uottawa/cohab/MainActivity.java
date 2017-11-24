951689ae4ce5c3a3f0001a509767a347801a3d50
zergoss <rroyl091@uottawa.ca>
2017-11-23 23:32:06 -0500

    Test new branch

--- application/app/src/main/java/ca/uottawa/cohab/MainActivity.java
+++ application/app/src/main/java/ca/uottawa/cohab/MainActivity.java
@@ -56,8 +56,8 @@ public class MainActivity extends AppCompatActivity
             fragmentManager.beginTransaction().replace(R.id.content_frame, new TaskList()).commit();
         } else if (id == R.id.nav_user_list) {
             fragmentManager.beginTransaction().replace(R.id.content_frame, new UserList()).commit();
-        } else if (id == R.id.nav_profil) {
-            fragmentManager.beginTransaction().replace(R.id.content_frame, new ProfilEdit()).commit();
+        } else if (id == R.id.nav_profile) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new ProfileEdit()).commit();
         } else if (id == R.id.nav_switch_user) {
             fragmentManager.beginTransaction().replace(R.id.content_frame, new SwitchUser()).commit();
         } else if (id == R.id.nav_log_off) {
