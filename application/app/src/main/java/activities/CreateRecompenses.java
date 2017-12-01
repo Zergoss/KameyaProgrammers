bdc252eb3c62a91e30a1c82f7fabc9ec7be237b6
TONYMPESSA <tmpes031@uottawa.ca>
2017-11-30 23:52:26 -0500

    create Recompense debut

---
+++ application/app/src/main/java/activities/CreateRecompenses.java
@@ -0,0 +1,17 @@
+package activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.uottawa.cohab.R;
+
+public class CreateRecompenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recompenses);
+    }
+
+
+}
