e1f0aa95b53c145031f3fa6b601b35498962ba68
cdemb104 <cdemb104@uottawa.ca>
2017-11-30 22:33:28 -0500

    Create task

---
+++ application/app/src/main/java/activities/CreateTask.java
@@ -0,0 +1,15 @@
+package activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.uottawa.cohab.R;
+
+public class CreateTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+    }
+}
