d80526281020b766095f6f61447145a6c0896a09
cdemb104 <cdemb104@uottawa.ca>
2017-12-02 05:59:58 -0500

    Task creation possible

--- application/app/src/main/java/activities/CreateTask.java
+++ application/app/src/main/java/activities/CreateTask.java
@@ -15,6 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -32,23 +33,22 @@ import sql.DatabaseHelper;
 
 public class CreateTask extends AppCompatActivity {
 
-    private static final  String TAG = "CreateTask";
 
     private TextInputLayout textInputLayoutTaskName, textInputLayoutTaskDescription,
                             textInputLayoutTaskPoint, textInputLayoutTaskDate,
                             textInputLayoutTaskAssignedUser;
     private TextInputEditText textInputEditTextTaskName, textInputEditTextTaskDescription,
                             textInputEditTextTaskPoint, textInputEditTextTaskDate,
-                            textInputEditTextTaskAssignedUser;
-    private Button pickDateButton;
+                            textInputEditTextTaskAssignedUser,getTextInputEditTextTaskDate;
 
     private DatabaseHelper databaseHelper;
     private List<User> userList;
     private Task newTask;
     private Input inputValidation;
+    private Button createButton;
 
-    private DatePickerDialog.OnDateSetListener myDateSetListener;
     private User connectedUser;
+    private ScrollView myScrollView;
 
 
 
@@ -56,6 +56,8 @@ public class CreateTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+        initObjects();
+        initViews();
     }
 
     private void initViews(){
@@ -70,35 +72,19 @@ public class CreateTask extends AppCompatActivity {
         textInputEditTextTaskDescription = (TextInputEditText) findViewById(R.id.textInputEditTaskDescription);
         textInputEditTextTaskPoint = (TextInputEditText) findViewById(R.id.textInputEditTaskPoint);
         textInputEditTextTaskAssignedUser = (TextInputEditText) findViewById(R.id.textInputEditTaskAssignedUser);
+        textInputEditTextTaskDate = (TextInputEditText) findViewById(R.id.textInputEditTaskDate);
 
-        pickDateButton = (Button) findViewById(R.id.pickDateButton);
-        pickDateButton.setOnClickListener(new View.OnClickListener() {
+        connectedUser = databaseHelper.getUser(getIntent().getStringExtra("creator"));
+
+        createButton = (Button) findViewById(R.id.startCreateTask);
+        createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(CreateTask.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
-                        , myDateSetListener,year,month,day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
+                postDataToSQLite();
             }
         });
 
-        myDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month+1;
-                Log.d(TAG,"onDateSet: mm/dd/yyyy" + month + "/" + day + "/" +year);
-
-                String date =  month + "/" + day + "/" +year;
-                pickDateButton.setText(date);
-            }
-        };
-
-        connectedUser = databaseHelper.getUser(getIntent().getStringExtra("creator"));
+        myScrollView = (ScrollView) findViewById(R.id.myScrollView);
 
 
     }
@@ -107,24 +93,25 @@ public class CreateTask extends AppCompatActivity {
         databaseHelper = new DatabaseHelper(this);
         inputValidation = new Input(this);
         newTask = new Task ();
+
     }
 
    private void postDataToSQLite() {
         if (!inputValidation.isInputEditTextFilled(textInputEditTextTaskName, textInputLayoutTaskName, "Insert task name")) {
             return;
         }
-        if (!inputValidation.isInputEditTextFilled(textInputEditTextTaskDescription, textInputLayoutTaskDescription, "")) {
+       /* if (!inputValidation.isInputEditTextFilled(textInputEditTextTaskDescription, textInputLayoutTaskDescription, "")) {
             textInputEditTextTaskDescription.setText("");
         }
         if (!inputValidation.isInputEditTextFilled(textInputEditTextTaskDate, textInputLayoutTaskDate, "")) {
-            textInputEditTextTaskDate.setText("0");
-        }
+
+        }*/
         if (!inputValidation.isInputEditTextFilled(textInputEditTextTaskPoint, textInputLayoutTaskPoint,"")) {
             textInputEditTextTaskPoint.setText("0");
         }
-       if (!inputValidation.isInputEditTextFilled(textInputEditTextTaskAssignedUser, textInputLayoutTaskAssignedUser,"")) {
+       /*if (!inputValidation.isInputEditTextFilled(textInputEditTextTaskAssignedUser, textInputLayoutTaskAssignedUser,"")) {
            textInputEditTextTaskAssignedUser.setText(null);
-       }
+       }*/
 
        //NOT Working : to check if the user to assign to has a task starting the same day
       /* if(inputValidation.isInputEditTextFilled(textInputEditTextTaskAssignedUser,textInputLayoutTaskAssignedUser,"") &&
@@ -141,25 +128,28 @@ public class CreateTask extends AppCompatActivity {
                 }
             }
        }*/
+       if (!databaseHelper.checkTask(textInputEditTextTaskName.getText().toString().trim())) {
+
+           newTask.setName(textInputEditTextTaskName.getText().toString().trim());
+           newTask.setPoints(0);
+           newTask.setCreator(connectedUser);
+
+
+           databaseHelper.addTask(newTask);
+
+           // Snack Bar to show success message that record saved successfully
+          Snackbar.make(myScrollView,"Task Created !", Snackbar.LENGTH_LONG).show();
+          emptyInputEditText();
+
+       } else {
+           // Snack Bar to show error message that record already exists
+           Snackbar.make(myScrollView,"Task name already exists!", Snackbar.LENGTH_LONG).show();
+           textInputEditTextTaskName.setText(null);
+       }
        }
 
 
-       if (!databaseHelper.checkTask(textInputEditTextTaskName.getText().toString().trim())) {
 
-            newTask.setName(textInputEditTextTaskName.getText().toString().trim());
-
-
-            databaseHelper.addUser(user);
-
-            // Snack Bar to show success message that record saved successfully
-            Snackbar.make(nestedScrollView, getString(R.string.success_message), Snackbar.LENGTH_LONG).show();
-            emptyInputEditText();
-
-        } else {
-            // Snack Bar to show error message that record already exists
-            Snackbar.make(nestedScrollView, getString(R.string.error_username_exists), Snackbar.LENGTH_LONG).show();
-            emptyInputEditText();
-        }
 
     private void getDataFromSQLite() {
         // AsyncTask is used that SQLite operation not blocks the UI Thread.
@@ -177,5 +167,13 @@ public class CreateTask extends AppCompatActivity {
                 taskRecyclerAdapter.notifyDataSetChanged();
             }*/
         }.execute();
+    }
+
+    private void emptyInputEditText() {
+        textInputEditTextTaskDate.setText(null);
+        textInputEditTextTaskAssignedUser.setText(null);
+        textInputEditTextTaskDescription.setText(null);
+        textInputEditTextTaskName.setText(null);
+        textInputEditTextTaskPoint.setText(null);
     }
 }
